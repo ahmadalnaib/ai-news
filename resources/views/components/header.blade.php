f3bd3f68cb0b71748d3380a0bf990f7ed0896073
ahmad.al-naib <alnaib888@gmail.com>
2024-05-16 13:25:13 +0200

    add meta and title

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,15 +1,45 @@
 <header class="text-gray-600 body-font border-b border-gray-100">
-    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
-      <a href="{{ route('news.index') }}" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg width="100" height="100" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M303.942 155.908L283.042 155.542L255.842 108.858L320.509 144.442C322.942 134.275 332.859 135.158 339.634 136.433C353.884 139.1 362.734 154.6 370.8 166.7C369.759 169.813 368.094 172.681 365.908 175.13C363.722 177.579 361.059 179.556 358.084 180.942C353.852 181.77 349.475 181.45 345.409 180.017C336.792 188.333 319.934 188.4 305.284 185.133M347.234 210.258C357.784 200.417 340.067 198.825 330.734 199.3C322.217 199.733 305.967 210.717 300.45 212.433L283.784 195.525M291.35 203.208L274.834 215.433M264.017 191.025C264.017 191.025 286.1 216.592 267.75 238.758C249.4 260.925 205.934 268.133 200.317 287.742M200.317 287.742H344.35C344.35 287.742 316.625 266.533 290.859 273.425C265.092 280.317 200.317 287.742 200.317 287.742Z" stroke="black" stroke-width="8.33333" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M204.167 280.792C204.167 280.792 211.817 267.408 200.167 261.35C188.517 255.292 169.392 260.508 167.683 246.833C165.975 233.158 154.758 187.5 182.1 146.358C209.442 105.217 276.733 144.708 276.733 144.708" stroke="black" stroke-width="8.33333" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M168.183 249.133C168.183 249.133 127.625 291.583 76.9834 291.133C26.3417 290.683 13.2501 241.317 50.5334 224.458C87.8167 207.6 138.467 191.467 113.1 167.967C87.7334 144.467 38.6001 129.3 38.6001 129.3C38.6001 129.3 53.2751 164.5 61.5334 165.258C69.7917 166.017 106.342 162.283 106.342 162.283" stroke="black" stroke-width="8.33333" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M336.108 165.733C339.56 165.733 342.358 162.935 342.358 159.483C342.358 156.032 339.56 153.233 336.108 153.233C332.657 153.233 329.858 156.032 329.858 159.483C329.858 162.935 332.657 165.733 336.108 165.733Z" fill="black"/>
-            </svg>
-            
-
-      </a>
-    
-  </div>
+  <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+    <!-- Primary Navigation Menu -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+            <div class="flex">
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('news.index')" :active="request()->routeIs('news.index')">
+                        {{ __('الرئيسية') }} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+                        </svg>
+                        
+                    </x-nav-link>
+                  
+                </div>
+             
+            </div>
+            <!-- Logo -->
+            <div class="shrink-0 flex items-center justify-end">
+                <a href="{{ route('news.index') }}">
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                </a>
+            </div>
+            <!-- Hamburger -->
+            <div class="-me-2 flex items-center sm:hidden">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('news.index')" :active="request()->routeIs('news.index')">
+              {{ __('الرئيسية') }}
+            </x-responsive-nav-link>
+        </div>
+    </div>
+</nav>
 </header>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,48 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>جربوع |  تطوير العالم العربي</title>
+    <meta property="og:title" content="Jerboa.ai - تطوير العالم العربي من خلال الذكاء الاصطناعي" />
+    <meta property="og:description" content="Jerboa.ai هو منصة مخصصة لتطوير العالم العربي من خلال الذكاء الاصطناعي. نقدم مقالات مفيدة، وموارد تعليمية، وأحدث التحديثات الإخبارية، ومقاطع فيديو مثيرة، ومجتمع تفاعلي لتعزيز الوعي وتعميق المعرفة في هذا المجال المتطور." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.jerboa.ai" />
+    <meta property="og:image" content="{{ asset('favicon_io/android-chrome-512x512') }}" />
+    <meta name="description" content="Jerboa.ai مخصصة لتطوير العالم العربي من خلال الذكاء الاصطناعي. تقدم منصتنا مقالات مفيدة، وموارد تعليمية، وآخر التحديثات الإخبارية، ومقاطع فيديو مشوقة، ومجتمع تفاعلي، بهدف المساهمة بشكل معنوي في نمو وازدهار العالم العربي في مجال التكنولوجيا والابتكار." />
+    <meta name="keywords" content="الذكاء الاصطناعي، التعليم، العالم العربي، تقنية، ابتكار" />
+    
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen ">
-         <x-header></x-header>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('favicon_io/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('favicon_io/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('favicon_io/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('favicon_io/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ url('favicon_io/safari-pinned-tab.svg') }}" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    
 
-            <div class="">
-                {{ $slot }}
-            </div>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen ">
+        <x-header></x-header>
+
+        <div class="">
+            {{ $slot }}
         </div>
-        <x-footer></x-footer>
-    </body>
+    </div>
+    <x-footer></x-footer>
+</body>
+
 </html>
